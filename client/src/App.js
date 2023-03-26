6d256defd53077986e7255847111563e83e3fb7a
PRINCE-PRASAD <princeprasad.code@gmail.com>
2023-03-26 21:42:27 +0530

    basic changes in app

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,11 @@ import { ethers } from "ethers";
 import FileUpload from "./components/FileUpload";
 import Display from "./components/Display";
 import Modal from "./components/Modal";
+import shareVideo from "./assets/share.mp4";
 
+import styled from "styled-components";
 
-
-import './App.css';
+import "./App.css";
 
 function App() {
   const [account, setAccount] = useState("");
@@ -19,12 +20,11 @@ function App() {
 
   useEffect(() => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
-// Provider help in read data from blockchain
-// window.ethereum help in inject the metamask
-// Signer help in write
+    // Provider help in read data from blockchain
+    // window.ethereum help in inject the metamask
+    // Signer help in write
     const loadProvider = async () => {
       if (provider) {
-
         // ---------------------------------------------------
         window.ethereum.on("chainChanged", () => {
           window.location.reload();
@@ -40,7 +40,7 @@ function App() {
         const address = await signer.getAddress();
         setAccount(address);
         let contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-// ------------------------------------------------------
+        // ------------------------------------------------------
         const contract = new ethers.Contract(
           contractAddress,
           Upload.abi,
@@ -48,7 +48,6 @@ function App() {
         );
         // created the instance for the contract
         // -----------------------------------------------
-
 
         // console.log(contract);
         setContract(contract);
@@ -71,12 +70,18 @@ function App() {
       )}
 
       <div className="App">
-        <h1 style={{ color: "white" }}>Image Keeper</h1>
-        <div className="bg"></div>
-        <div className="bg bg2"></div>
-        <div className="bg bg3"></div>
+        <h1 style={{ color: "White" }}>Image Keeper</h1>
 
-        <p style={{ color: "white" }}>
+        {/* ---------------------------------------- */}
+        <div className="videoContainer">
+          <video className="videoTag" autoPlay loop muted>
+            <source src={shareVideo} type="video/mp4" />
+          </video>
+        </div>
+        <Overlay />
+        {/* ------------------------------------ */}
+        
+        <p style={{ color: "White" }}>
           Account : {account ? account : "Account Not connected"}
         </p>
         <FileUpload
@@ -86,9 +91,23 @@ function App() {
         ></FileUpload>
         <Display contract={contract} account={account}></Display>
       </div>
+      <Overlay />
     </>
   );
 }
 
-
+const Overlay = styled.div`
+  background-color: rgba(0, 0, 0, 0.4);
+  bottom: 0;
+  box-shadow: inset 0 0 5rem rgba(0, 0, 0, 0.5);
+  left: 0;
+  min-height: 100%;
+  min-width: 100%;
+  object-fit: cover;
+  object-position: center;
+  position: fixed;
+  right: 0;
+  top: 0;
+  z-index: -1;
+`;
 export default App;
